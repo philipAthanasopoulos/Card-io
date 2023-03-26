3ccb41d5d1dfa3094f2818d3a2f495ceb9fcdd13
Filippos Athanasopoulos Antyras <80889555+FIlippos-Athanasopoulos-Antyras@users.noreply.github.com>
2023-03-26 14:27:20 +0300

    init 2

---
+++ CardDealer.java
@@ -0,0 +1,16 @@
+package ask2;
+
+public class CardDealer {
+    private int numOfCards;
+    private int numOfGroups;
+    
+
+    public CardDealer(){
+
+    }
+
+    public void createCardDeck(int numOfCards , int numOfGroups){
+
+    }
+    
+}
---
+++ Player.java
@@ -0,0 +1,15 @@
+package ask2;
+
+public class Player{
+    private String name;
+    private int score;
+
+    public Player(String name){
+        this.name = name;
+        this.score = 0;
+    }
+
+    public void play(int posx , int posy){
+        
+    }
+}
