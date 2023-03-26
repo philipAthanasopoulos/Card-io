95aeb649034effb30af939b978578e3225d9ded3
Filippos Athanasopoulos Antyras <80889555+FIlippos-Athanasopoulos-Antyras@users.noreply.github.com>
2023-03-26 14:29:23 +0300

    done

--- Table.java
+++ Table.java
@@ -1,13 +1,12 @@
 package ask2;
 
 public class Table {
-    private int[][] table;
     private int numOfCards;
     private int numOfGroups;
 
 
     public void createCardGroups(int numOfCards , int numOfGroups){
-        
+
     }
 
 }
