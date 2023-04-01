b01794c44bc87e2dd68e854f7fb25446e9eec7af
tsoumpaklas <62931343+tsoumpaklas@users.noreply.github.com>
2023-04-01 16:56:45 +0300

    updated
    updated

--- ask2/CardDealer.java
+++ ask2/CardDealer.java
@@ -10,10 +10,12 @@ public class CardDealer {
     private int totalNumOfCards;
     private int numOfGroups;
     private List<CardGroup> groups = new ArrayList<CardGroup>();
+    //Creating a list that holds each size of Group
+    List<Integer> Groups = new ArrayList<>();
+    //Here i declare a randomIndex from Groups's list
     private int randomIndex;
     private int groupSize=0;
     private int remainingCards;
-    List<Integer> Groups = new ArrayList<>();
     Random randomGroups = new Random();
 
     /*public CardDealer(){
@@ -33,12 +35,13 @@ public class CardDealer {
 
     public void createCardDeck(int totalNumOfCards , int numOfGroups){
         //Every group has at least 2 cards, so i subtract them from totalNumOfCards
-        //Px, an exo 40 cards kai 4 groups, tote remainingCards = 40 -2*4
-        //Exo valei ston Constructor tou CardGroup to groupSize apo default na einai 2
+        //For instance, if i have 40 cards and 4 groups, then remainingCards = 40 -2 * 4
+        //I have placed groupSize of CardGroup's Constructor to be 2 by default
         this.groupSize = numOfGroups;
          remainingCards = totalNumOfCards - 2 * numOfGroups;
         //System.out.println("Malakas");
         Random random = new Random();
+        //If remaining cards are 0, that means that i have groups with exactly 2 cards.
         if(remainingCards == 0){
             for(int i=0; i<numOfGroups; i++){
                 //System.out.println("Kourampies");
@@ -46,33 +49,40 @@ public class CardDealer {
                 Groups.add(new CardGroup(0).getCardsGroup());
             }
         }
+        //If totalNumOfCards/numOfGroups < 2 -> Groups are too many
         else if(totalNumOfCards/numOfGroups < 2){
             System.out.println("Groups are too many");
             askingDealer();
         }
+        //(Example. If i have 21/10, then 21/10=2 but i have one remaining or if i have 12/4 > 2) ---> that means
+        //that i will add cards to the default cardGroup creation.
         else if(totalNumOfCards/numOfGroups == 2 && totalNumOfCards % numOfGroups !=0 || totalNumOfCards/numOfGroups > 2 ){
             int cardsForGroup;
             for(int i=0; i<numOfGroups; i++){
                 if(i==numOfGroups - 1){
-                    //Ean vretho sto teleutaio group tote ta cards tou Group tha einai ta remaining
+                    //If i go to the last group, then the cards of Group will be the remaining
                     cardsForGroup = remainingCards;
                 }
                 else{
                     //System.out.println("Malakas 2");
-                    //Edo epilego enan arithmo apo 0-remainingCards
+                    //Here, i choose a number between 0 - remainingCards
                     cardsForGroup = random.nextInt(remainingCards);
                     remainingCards -= cardsForGroup;
                 }
                 //edo ta kano add stin lista ton stoivadon. To 1o stoixeio tha einai i proti
                 //stoivada me ta tade xaraktiristika.
+                //Here i add them to the list of stacks. The first element is the first stack
+                //with these specific characteristics.
                 if (remainingCards==0){
                     //System.out.println("Lene pos eimai trelos");
                     groups.add(new CardGroup(remainingCards));
+                    //Here i add them to the list of Groups that are Integers
                     Groups.add(new CardGroup(remainingCards).getCardsGroup());
                 }
                 else{
                     //System.out.println("Helllo there");
                     groups.add(new CardGroup(cardsForGroup));
+                    //Here i add them to the list of Groups that are Integers
                     Groups.add(new CardGroup(cardsForGroup).getCardsGroup());
                 }
                 
