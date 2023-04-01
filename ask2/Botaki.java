633edd1545bfb4bacd92ff563a59cae0cf1ccd48
Filippos Athanasopoulos Antyras <80889555+FIlippos-Athanasopoulos-Antyras@users.noreply.github.com>
2023-04-01 20:55:26 +0300

    added more code

--- ask2/Botaki.java
+++ ask2/Botaki.java
@@ -1,5 +1,29 @@
 
 
 public class Botaki extends Player {
+    private int groupToPlay;
+    private int cardsToRemove;
+
     
+    public void calculateBestMove(CardDeck currentDeck){
+
+    }
+
+    public int getGroupToPlay() {
+        return this.groupToPlay;
+    }
+
+    public void setGroupToPlay(int groupToPlay) {
+        this.groupToPlay = groupToPlay;
+    }
+
+    public int getCardsToRemove() {
+        return this.cardsToRemove;
+    }
+
+    public void setCardsToRemove(int cardsToRemove) {
+        this.cardsToRemove = cardsToRemove;
+    }
+
+
 }
