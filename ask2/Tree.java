3a73f6eb07540c471f36b5f601e4e5bef3813f47
Filippos Athanasopoulos Antyras <80889555+FIlippos-Athanasopoulos-Antyras@users.noreply.github.com>
2023-04-10 09:55:30 +0300

    added tree data stucture

---
+++ ask2/Tree.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+public class Tree{
+    int data;
+    List<Tree> children = new LinkedList<>();
+
+    Tree(int data){
+        this.data = data;
+    }
+
+    Tree(int data, List<Tree> children){
+        this.data = data;
+        this.children = children;
+    }
+
+    public static void printTree(Tree root){
+        if(root == null) return;
+        Queue<Tree> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while(!queue.isEmpty()){
+            int length = queue.size();
+            for(int i = 0 ; i < length; i++){
+                Tree node = queue.poll();
+                System.out.print(node.data + " ");
+                for(Tree item : node.children){
+                    queue.offer(item);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Tree root = new Tree(1);
+        root.children.add(new Tree(2));
+        root.children.add(new Tree(2));
+        root.children.add(new Tree(2));
+        root.children.get(0).children.add(new Tree(3));
+        root.children.get(1).children.add(new Tree(3));
+        root.children.get(0).children.add(new Tree(3));
+
+
+        printTree(root);
+
+
+        
+    }
+}
